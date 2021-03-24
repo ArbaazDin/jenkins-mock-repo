5928348d609d46bd7cb04777f2ec3127c9f274a4
ArbaazDin <arbaazdin@gmail.com>
2021-03-24 12:38:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+
+    res.status(200).json({
+        status : "SUCCESS",
+        message : 'Good Morning!!'
+    })
+    return;
+});
+
+app.get('/hello', (req, res) => res.send('Hello World !!'));
+
+
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
