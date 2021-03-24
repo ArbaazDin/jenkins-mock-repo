76e35115cc63e313b64e6913ebe0493e229493da
ArbaazDin <arbaazdin@gmail.com>
2021-03-24 18:19:25 +0530

    14

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 ssh ubuntu@13.127.62.187 <<EOF
     cd ~/jenkins-mock-repo
+    echo "printing files"
+    ls 
     git pull origin main
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
     . ~/.nvm/nvm.sh
     nvm install v15.11.0
     npm install
     npm install -g nodemon pm2
+    echo "printing files"
+    ls
     pm2 restart ecosystem.config.js
     exit
 EOF
