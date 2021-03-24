54249eb1eb8d1da10147928d5190405bab12e7ad
ArbaazDin <arbaazdin@gmail.com>
2021-03-24 17:48:10 +0530

    10

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ describe("node js app", function() { // for domain or per module
 
     describe("hello request", function() { // per url
 
-        const localhostUrl = "http://localhost:8080/hello";
+        const localhostUrl = "http://localhost:3000/hello";
 
         it("hello request should return status code 200 ", function(done){ // multiple test for individual request
             request(localhostUrl, function(error, response, body ) {
