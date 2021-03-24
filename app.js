bda00b819f5c914ed276ec8370f5f6f1c1f7b137
ArbaazDin <arbaazdin@gmail.com>
2021-03-24 17:13:14 +0530

    6

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     return;
 });
 
-app.get('/hello', (req, res) => res.status(200).send('Hello!!'));
+app.get('/hello', (req, res) => res.status(200).send('Hello Hi!!'));
 
 
 app.listen(port);
